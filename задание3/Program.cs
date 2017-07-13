02a63cf10a14ba3b8b8b9967a1f7612e2a2f6746
Дом <@DESKTOP-7GF1H2I>
2017-07-13 19:01:28 +0500

    done

--- задание3/Program.cs
+++ задание3/Program.cs
@@ -10,6 +10,25 @@ namespace задание3
     {
         static void Main(string[] args)
         {
+            double x = 0,y=0;
+            VvodPoint("координата х", out x);
+            VvodPoint("координата у", out y);
+            if ((x * x + y * y <= 1) && (x * x + y * y >= 0.25)) Console.WriteLine("Точка принадлежит");
+                else Console.WriteLine("Точка не принадлежит");
+            Console.ReadKey();                           
+        }
+        static double VvodPoint(string s, out double n)//Вввод размера массива
+        {
+            bool ok;
+            string buf;
+            do
+            {
+                Console.Write(s + " = ");
+                buf = Console.ReadLine();
+                ok = double.TryParse(buf, out n);
+                if (!ok) Console.WriteLine("Введите " + s + " заново");
+            } while (!ok);
+            return n;
         }
     }
 }
